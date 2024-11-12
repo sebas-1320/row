469d796c91aa2b3c4f4912d7797a565321f29625
sebas-1320 <sebastiantv305@gmail.com>
2024-11-12 11:26:16 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,107 @@
+
+function cambiarTexto(){
+
+    const selectorTitulo = document.getElementById('titulo');
+    const nombreUsuario = prompt("Ingrese su nombre");
+    const contenedor = document.getElementById('cont-general');
+    // cambiando contenido de una eetiqueta desde js
+    selectorTitulo.innerText = "Óscar Castro";
+    selectorTitulo.innerHTML = "<strong>USUARIO:</strong> " + nombreUsuario;
+    //cambiando estilos de una etiqueta dede js
+    selectorTitulo.style.backgroundColor = "white";
+    selectorTitulo.style.color = "#000";
+    selectorTitulo.style.padding = "20px"
+    //cambiando estrutura  de una etiqueta desde js
+    contenedor.classList.add('dia');
+}
+
+function Circulo(){
+    const selectorTitulo = document.getElementById('titulo');
+    const circulo = document.getElementById('figura');
+    const redondo = document.getElementById('cont-general');
+    selectorTitulo.innerText = " Circulo";
+    circulo.style.borderRadius = "50%";
+    circulo.style.backgroundColor = "#8400ff";
+}
+
+function Rombo(){
+    const selectorTitulo = document.getElementById('titulo');
+    const rombo = document.getElementById('figura');
+    selectorTitulo.innerText = " Rombo";
+    rombo.style.rotate = "45deg"
+    rombo.style.borderRadius = "0"
+    rombo.style.backgroundColor = "#460088";
+}
+
+function FondoColor(){
+    const fondo = document.getElementById('cont-general');
+    fondo.style.background = "white";
+}
+
+function FondoImagen(){
+    const fondoImg = document.getElementById('cont-general');
+    fondoImg.style.backgroundImage  = "url('img/fondo incap 2023.jpeg')";
+}
+
+function BtnTop(){
+    const selectorTitulo = document.getElementById('titulo');
+    const figura = document.getElementById('figura');
+    figura.style.position = "absolute";
+    figura.style.top = 0;
+    figura.style.right = 0;
+    figura.style.left = 0;
+}
+
+function BtnLeft(){
+    const selectorTitulo = document.getElementById('titulo');
+    const figura = document.getElementById('figura');
+    figura.style.position = "absolute";
+    figura.style.top = 0;
+    figura.style.left = 0;
+    
+}
+
+function BtnRight(){
+    const selectorTitulo = document.getElementById('titulo');
+    const figura = document.getElementById('figura');
+    figura.style.position = "absolute";
+    figura.style.top = 0;
+    figura.style.right = 0;
+}
+
+function BtnBottom(){
+    const selectorTitulo = document.getElementById('titulo');
+    const figura = document.getElementById('figura');
+    figura.style.position = "absolute";
+    figura.style.top = 1000;
+    figura.style.right = 0;
+    figura.style.left = 0;
+}
+
+function BtnEstrella(){
+    const selectorTitulo = document.getElementById('titulo');
+    const estrella = document.getElementById('figura');
+    const redondo = document.getElementById('cont-general');
+    selectorTitulo.innerText = " Estrella";
+    estrella.style.clipPath = "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+    estrella.style.backgroundColor = "#8400ff";
+}
+function (){
+
+}
+
+function (){
+    
+}
+function (){
+    
+}
+function Panel_Lateral(){
+    const panel = document.getElementById("panel_Lateral");
+    panel.classList.toggle("active");
+}
+
+function Panel_Superior(){
+    const panelo = document.getElementById("panel_Superior");
+    panelo.classList.toggle("active");
+}
